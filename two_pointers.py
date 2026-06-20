0cb8fc5772380f608a5575c7a1042fe2d9bf817a
Pratham Bahuguna <pbahu@uic.edu>
2026-06-20 19:14:03 +0530

    Two sum added

---
+++ two_pointers.py
@@ -0,0 +1,28 @@
+# --- Valid Palindrome(using alphanum and extra memory) ---
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstr=""
+
+        for c in s:
+            if c.isalnum():
+                newstr+=c.lower()
+        return newstr == newstr[::-1]
+        
+# --- Valid Palindrome(two pointers the better way) ---
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
